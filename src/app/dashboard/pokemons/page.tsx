8f9c612b4c9fd3d0ea35b536490f39a54291aad4
fmoraless <franticzero@gmail.com>
2025-01-03 13:30:17 -0300

    Add PokemonGrid and PokemonCard components, and implement PokemonsPage with dynamic fetching

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,7 +1,35 @@
-export default function PokemonsPage() {
+import { PokemonGrid, PokemonsResponse, SinglePokemon } from "@/app/pokemons";
+
+/* 
+TODO: contionuar desde Rutas dinámicas - Argumentos por URL
+
+*/
+
+const getPokemons = async (
+  limit = 20,
+  offset = 0
+): Promise<SinglePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json());
+
+  const pokemons = data.results.map((pokemon) => ({
+    id: pokemon.url.split("/").at(-2)!,
+    name: pokemon.name,
+  }));
+  //throw new Error("Error al cargar los pokemons");
+  return pokemons;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons(151);
+
   return (
-    <div>
-      <h1>Hello Pokemons</h1>
+    <div className="flex flex-col">
+      <span className="text-5xl my-2">
+        Listado Pokemons <span>Estatico</span>
+      </span>
+      <PokemonGrid pokemons={pokemons} />
     </div>
   );
 }
---
+++ src/app/pokemons/index.ts
@@ -0,0 +1,4 @@
+export { PokemonGrid, PokemonCard } from "./components/PokemonGrid";
+
+export type { PokemonsResponse } from "./interfaces/pokemons-response";
+export type { SinglePokemon } from "./interfaces/single-pokemon";
