299535eaa3a73b22315b100a36a368627fb61e3c
fmoraless <franticzero@gmail.com>
2025-01-03 23:26:42 -0300

    fix: Address dynamic routing issue in PokemonPage component

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,6 +1,8 @@
 interface Props {
   params: { id: string };
 }
+
+////TODO: repasar rutas diunamicas, ya que hubo error.
 export default function PokemonPage({ params }: Props) {
   console.log({ params });
   return (
