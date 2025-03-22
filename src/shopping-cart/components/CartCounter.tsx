832bec1bde52d49139a5f86bab42da9eadfb7f72
fmoraless <franticzero@gmail.com>
2025-03-22 00:42:17 -0300

    feat: Implement Redux for CartCounter component and add counterSlice

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,37 +1,50 @@
 "use client";
-import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/store";
+import {
+  addOne,
+  initCounterState,
+  subtractOne,
+} from "@/store/counter/counterSlice";
+import { useEffect, useState } from "react";
 
 interface Props {
   value?: number;
 }
 
 export const CartCounter = ({ value = 0 }: Props) => {
-  const [counter, setCounter] = useState(value);
+  const counter = useAppSelector((state) => state.counter.count);
+  const dispatch = useAppDispatch();
 
-  const increment = () => {
-    setCounter(counter + 1);
-  };
+  useEffect(() => {
+    dispatch(initCounterState(value));
+  }, [dispatch, value]);
 
-  const decrement = () => {
-    if (counter === 0) return;
-    setCounter(counter - 1);
-  };
+  // const [counter, setCounter] = useState(value);
+
+  // const increment = () => {
+  //   setCounter(counter + 1);
+  // };
+
+  // const decrement = () => {
+  //   if (counter === 0) return;
+  //   setCounter(counter - 1);
+  // };
 
   return (
     <>
       <span className="text-9xl">{counter}</span>
       <div className="flex">
         <button
-          onClick={increment}
+          onClick={() => dispatch(addOne())}
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
         >
           +1
         </button>
         <button
-          onClick={decrement}
+          onClick={() => dispatch(subtractOne())}
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
         >
-          +1
+          -1
         </button>
       </div>
     </>
